08171ede4ded8c291b1b71f6e10f3a51f498d6a1
rafi <rafi@mixorg.com>
2015-12-31 10:32:47 +0530

    my initial first commit

--- src/com/ptr/foldingdrawerlayout/sample/AnimalFragment.java
+++ src/com/ptr/foldingdrawerlayout/sample/AnimalFragment.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ //rafiiiiiii
 package com.ptr.foldingdrawerlayout.sample;
 
 import java.util.Locale;
